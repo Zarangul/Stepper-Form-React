72fb358365e201cdc4d17b2f16fe2e20c9e9ab67
Zarangul <zarangulbabazade@gmail.com>
2023-04-03 11:52:08 +0400

    feat/buttons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import {Stepper} from './components/Stepper'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       
+      <Stepper>Hello</Stepper>
     </div>
   );
 }
